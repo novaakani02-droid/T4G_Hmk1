636fdcb7ac295be3a0fcefe0a20680952a3a6a17
novaakani02-droid <novaakani02@gmail.comgit config --global user.name>
2026-07-20 12:57:23 +0000

    linked external js to homepage

---
+++ src/script.js
@@ -0,0 +1,5 @@
+let studentname =prompt("Please enter your name");
+let studentage =prompt("Please enter your age");
+let studentemail =prompt("Please enter your email");
+let message = `Hi ${studentname}, you are ${studentage} years old and we'll keep in touch via your email: ${studentemail}`;
+alert(message);
