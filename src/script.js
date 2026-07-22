be17b0f2a73f2a92e771f27a05b198275314ecad
novaakani02-droid <novaakani02@gmail.comgit config --global user.name>
2026-07-22 08:34:11 +0000

    basic mathematics with js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,56 @@
-let studentname =prompt("Please enter your name");
+let studentname =prompt("Please enter your name"); //you enter your name
+
 let studentage =prompt("Please enter your age");
+
 let studentemail =prompt("Please enter your email");
+
 let message = `Hi ${studentname}, you are ${studentage} years old and we'll keep in touch via your email: ${studentemail}`;
 alert(message);
+
+
+let name = "Victoria Akani";
+console.log(name);
+
+let age = 18
+console.log(age);
+
+
+
+let a = 10
+let b = 20
+// addition
+let addnumbers = a + b
+console.log(addnumbers); //output - 20
+
+// subtraction
+let difference = b - a
+console.log(difference); //output - 10
+
+//division
+let quotient = b/a
+console.log(quotient); //output - 2
+
+//multiplication
+let product = a * b
+console.log(product); // output - 200
+
+//finding remainder of 15 % 4
+let remainder=15 % 4
+console.log(remainder);
+
+//Boolean value
+let hasEaten = true;
+console.log(hasEaten);
+
+// Number value
+let numberValue = 100;
+console.log(typeof numberValue);
+
+// J. String value
+let stringValue = "Hi Tori";
+console.log(typeof stringValue);
+
+ 
+
+
+
